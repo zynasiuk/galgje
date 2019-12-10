0a19afedb8b1ab3b3e8478141a30fbef8e3b7b68
zynasiuk <54539663+zynasiuk@users.noreply.github.com>
2019-12-10 11:06:48 +0100

    Working, but with mistake
    .find() in index_number variable causes problem; it must be replaced by other solution OR removed directly from woord in each iteration

---
+++ galgje.py
@@ -0,0 +1,65 @@
+import random
+import string
+alfa = string.ascii_uppercase
+vijf_te_raden_worden = ['black', 'red', 'pink', 'blue', 'white']
+kansen = 5
+invoer = ""
+guessing_word=[]
+list_of_tries=[]
+geraden = 0
+play = True
+
+
+while len(vijf_te_raden_worden) > 0 and play == True:
+    te_raden = random.choice(vijf_te_raden_worden)
+    woord = te_raden.upper()
+
+    for i in woord:
+        i = "_"
+        guessing_word.append(i)
+
+    guessing_word_str = ''.join([str(element) for element in guessing_word])
+    print(woord)
+
+    print("I'm thinking about some color, it's... Try to guess name of it! Try with 1 letter.")
+
+    while woord != guessing_word_str and kansen > 0:
+        print("You have " +str(kansen)+ " tries. " + guessing_word_str)
+        invoer = (input("Which color it could be? ")).upper()
+
+        if len(invoer) == 1:
+            if invoer in alfa:
+                for i in woord:
+                    if i == invoer:
+                        index_number = woord.find(i)
+                        guessing_word[index_number]= i
+                        kansen += 1
+
+                kansen -= 1
+                guessing_word_str = ''.join([str(element) for element in guessing_word])
+                list_of_tries.append(invoer)
+                print("You did try already: " + str(list_of_tries) + " times.")
+            else:
+                print("not a letter")
+        else:
+            print("ONLY 1 LETTER!")
+
+    if guessing_word_str == woord :
+        print("Yes, it is "+ guessing_word_str +"! Congrats!")
+        geraden += 1
+        vijf_te_raden_worden.remove(te_raden)
+        guessing_word.clear()
+        list_of_tries.clear()
+        if len(vijf_te_raden_worden) > 0:
+            new_game=(input("One more time? Press any key for yes / N for no: ")).upper()
+            if new_game == "N" and len(vijf_te_raden_worden) > 0:
+                play == False
+                break
+
+
+    else:
+        print("Sorry, you lost!")
+        play = False
+
+if len(vijf_te_raden_worden) == 0:
+    print("Yaaaayyyyyy! You won " + str(geraden) + " times ! ! ! You are THE WINNER!!!")
